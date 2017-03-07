5427f4ebbb499b2b048f7185bb693cc7a806b5d0
lilvina <davinaleo2007@yahoo.com>
2017-03-06 17:00:10 -0800

    still working on udacity

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,12 +29,7 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    Use the Fetch API to GET a URL.
-    Return the fetch.
-
-    Your code goes here!
-     */
+     return fetch(url);
   }
 
   /**
@@ -48,6 +43,9 @@ Instructions:
 
     Your code goes here!
      */
+     return get(url).then(function(response){
+      return response.json();
+     });
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -59,5 +57,14 @@ Instructions:
     Your code goes here too!
      */
     // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function(response){
+      addSearchHeader(response.query);
+      console.log(response);
+    })
+    .catch(function(error){
+      addSearchHeader('unknown');
+      console.log(error);
+    });
   });
 })(document);
